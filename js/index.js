1bbd727671caa0a5c0f08a2260a0c935026a4b49
Knight Khode <elmirhamude@gmail.com>
2021-06-16 23:34:34 -0700

    clean up folder structure

---
+++ js/index.js
@@ -0,0 +1,26 @@
+var options = {
+    strings: ['I Am Mohammed Elmir','I Am A Web Devoloper'],
+    typeSpeed: 40
+};
+
+var typed = new Typed('#typed', options);
+
+// hamburger
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
